cb516aadc2324bdd3cb0c0e16cdf4f1395e70799
ivnblv <ivnb.web@gmail.com>
2019-08-20 23:41:28 +0300

    Updates

---
+++ precache-manifest.1ee96d0ebb50f604efa31cfd8e125a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1707038be73067047853facad9731fb",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "6d51afd29b476b354376",
+    "url": "/trivia/static/css/main.bca66870.chunk.css"
+  },
+  {
+    "revision": "9bc39d9f9ce731c47931",
+    "url": "/trivia/static/js/2.d077bff5.chunk.js"
+  },
+  {
+    "revision": "6d51afd29b476b354376",
+    "url": "/trivia/static/js/main.cec30ea7.chunk.js"
+  },
+  {
+    "revision": "906f7b546199d04a0678",
+    "url": "/trivia/static/js/runtime~main.a8a14f72.js"
+  }
+]);
